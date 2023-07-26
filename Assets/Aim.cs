2d65daed37813cfc64485aaa5410080eb506f7ca
vitorsti <vitorstigliani@gmail.com>
2023-07-26 17:55:16 -0300

    add m2hb model

--- Assets/Aim.cs
+++ Assets/Aim.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class Aim : MonoBehaviour
 {
-    [SerializeField]
-    GameObject _bullet;
+    ///[SerializeField]
+    //GameObject _bullet;
     [SerializeField]
     Vector3 _screenPosi;
     [SerializeField]
     Vector3 _worldPosi;
     [SerializeField]
     Transform _target;
-    [SerializeField]
-    Transform _bulletSpawn;
+    //[SerializeField]
+    //Transform _bulletSpawn;
     [SerializeField]
     float zOffset;
     float _mouseX = 0;
@@ -23,6 +23,9 @@ public class Aim : MonoBehaviour
     [SerializeField]
     float _fireRate;
     bool shoot;
+
+    [SerializeField]
+    bool useLookAt;
     // Start is called before the first frame update
     void Start()
     {
@@ -36,62 +39,64 @@ public class Aim : MonoBehaviour
     void Update()
     {
         #region Rotation With LookAt
-        
-        //LookAt
-        float h = Input.GetAxis("Mouse X");
-        float v = Input.GetAxis("Mouse Y");
+        if (useLookAt)
+        {
+            //LookAt
+            float h = Input.GetAxis("Mouse X");
+            float v = Input.GetAxis("Mouse Y");
 #if UNITY_EDITOR
-        //Debug.Log("mouse x: " + h);
-        //Debug.Log("mouse y: " + v);
+            //Debug.Log("mouse x: " + h);
+            //Debug.Log("mouse y: " + v);
 #endif
-        _screenPosi = Input.mousePosition;
-        _screenPosi.z = Camera.main.nearClipPlane + zOffset;
-        _worldPosi = Camera.main.ScreenToWorldPoint(_screenPosi);
-        _target.position = _worldPosi;
-        Quaternion OriginalRot = transform.rotation;
-        transform.LookAt(_target, Vector3.up);
+            _screenPosi = Input.mousePosition;
+            _screenPosi.z = Camera.main.nearClipPlane + zOffset;
+            _worldPosi = Camera.main.ScreenToWorldPoint(_screenPosi);
+            _target.position = _worldPosi;
+            Quaternion OriginalRot = transform.rotation;
+            transform.LookAt(_target, Vector3.up);
 
-        Quaternion NewRot = transform.rotation;
-        transform.rotation = OriginalRot;
-        transform.rotation = Quaternion.Lerp(transform.rotation, NewRot, sensitivity * Time.deltaTime);
+            Quaternion NewRot = transform.rotation;
+            transform.rotation = OriginalRot;
+            transform.rotation = Quaternion.Lerp(transform.rotation, NewRot, sensitivity * Time.deltaTime);
 
-        float ry = transform.eulerAngles.y;
-        float rx = transform.eulerAngles.x;
+            float ry = transform.eulerAngles.y;
+            float rx = transform.eulerAngles.x;
 
-        if (ry >= 180) ry -= 360;
-        if (rx >= 180) rx -= 360;
+            if (ry >= 180) ry -= 360;
+            if (rx >= 180) rx -= 360;
 
-        transform.eulerAngles = new Vector3(
-            Mathf.Clamp(rx, -10, 10),
-            Mathf.Clamp(ry, -60, 60),
-            0
-        );
+            transform.eulerAngles = new Vector3(
+                Mathf.Clamp(rx, -10, 10),
+                Mathf.Clamp(ry, -60, 60),
+                0
+            );
+        }
         #endregion
 
         #region Rotation Without LookAt
+        if (!useLookAt)
+        {
+            //rotation without lookat
+            _mouseX += Input.GetAxis("Mouse X") * sensitivity;
+            _mouseX = Mathf.Clamp(_mouseX, -60, 60);
 
-        /*
-        //rotation without lookat
-        _mouseX += Input.GetAxis("Mouse X") * sensitivity;
-        _mouseX = Mathf.Clamp(_mouseX, -60, 60);
+            _mouseY -= Input.GetAxis("Mouse Y") * sensitivity;
+            _mouseY = Mathf.Clamp(_mouseY, -10, 10);
 
-        _mouseY -= Input.GetAxis("Mouse Y") * sensitivity;
-        _mouseY = Mathf.Clamp(_mouseY, -10, 10);
+            transform.localRotation = Quaternion.Euler(_mouseY, _mouseX, 0);
 
-        transform.localRotation = Quaternion.Euler(_mouseY, _mouseX, 0);
+            float ry = transform.eulerAngles.y;
+            float rx = transform.eulerAngles.x;
 
-        float ry = transform.eulerAngles.y;
-        float rx = transform.eulerAngles.x;
+            if (ry >= 180) ry -= 360;
+            if (rx >= 180) rx -= 360;
 
-        if (ry >= 180) ry -= 360;
-        if (rx >= 180) rx -= 360;
-
-        transform.eulerAngles = new Vector3(
-            Mathf.Clamp(rx, -10, 10),
-            Mathf.Clamp(ry, -60, 60),
-            0
-        );
-        */
+            transform.eulerAngles = new Vector3(
+                Mathf.Clamp(rx, -10, 10),
+                Mathf.Clamp(ry, -60, 60),
+                0
+            );
+        }
         #endregion
   
     }
